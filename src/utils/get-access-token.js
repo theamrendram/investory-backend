b032ea424b53c01b5b0f56378ac114a2cc098e04
KumarAmrendram <amrendramkumar@gmail.com>
2025-11-06 00:48:02 +0530

    added firebase-admin.json content in .env

--- src/utils/get-access-token.js
+++ src/utils/get-access-token.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 function getAccessToken() {
+  if(process.env.NODE_ENV === "production") {
+    return process.env.UPSTOX_ACCESS_TOKEN;
+  }
   return JSON.parse(fs.readFileSync(".upstox_token.json"));
 }
 
